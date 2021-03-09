98fa8907d64d069a8eb210c30f3255ce79571390
asma <asma.rw09@gmail.com>
2021-03-09 17:29:03 +0100

    suite bonus

--- jour-2/jour02.js
+++ jour-2/jour02.js
@@ -78,4 +78,61 @@ else {
     console.log("So close !")
 }
 
+var month = "marchr";
 
+switch (month) {
+	case "january": 
+		console.log(" winter!");
+		break;
+	case "february": 
+		console.log("winter!");
+		break;
+	case "march": 
+		console.log("spring!");
+		break;
+	case "april": 
+		console.log("spring!");
+		break;
+    case "mai": 
+		console.log("spring!");
+		break;
+	case "june": 
+		console.log("summer!");
+		break;
+	case "jully": 
+		console.log("summer!");
+		break;
+	case "august": 
+		console.log("summer!");
+		break;
+    case "september": 
+		console.log("automn!");
+		break;
+	case "october": 
+		console.log("automn!");
+		break;
+	case "november": 
+		console.log("automn!");
+		break;
+	case "december": 
+		console.log("winter!");
+		break;
+	default: 
+		console.log("That's not a month...");
+}
+
+var roundedNumber = 6.6;
+
+var number = Math.floor(roundedNumber); 
+
+var results = roundedNumber - number;
+
+console.log(results);
+
+if (results <= 0.5)
+{
+    console.log(Math.floor(roundedNumber));
+}
+else {
+    console.log(Math.ceil(roundedNumber));
+}
