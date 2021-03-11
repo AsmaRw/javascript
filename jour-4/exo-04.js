cba0a91936498900bfd9664612760d827f7d1455
asma <asma.rw09@gmail.com>
2021-03-11 17:03:18 +0100

    exo-5.js

---
+++ jour-4/exo-04.js
@@ -0,0 +1,20 @@
+// ## 04 - Compare
+
+// - Créez une fonction `compare` qui reçoit deux paramètres `num1` et `num2`
+// - Dans la fonction, à l'aide d'une condition, affichez selon les cas :
+//     - "`num1` is bigger" si `num1` est plus grand
+//     - "`num2` is bigger" si `num2` est plus grand
+//     - "both are the same" si ils sont égaux
+// - Appelez votre fonction plusieurs fois avec des chiffres différents pour vérifier les résultats
+
+function compare(num1, num2){
+    if (num1 > num2){
+        console.log("num1 is bigger");
+    } else if (num2 > num1){
+        console.log("num2 is bigger");
+    } else if (num1 === num2) {
+        console.log("both are same");
+    }
+}
+
+compare(5, 5);
---
+++ jour-4/exo-5.js
@@ -0,0 +1,13 @@
+// ## 05 - Add Up
+
+// - Créez une fonction `addUp` qui reçoit un paramètre `num`
+// - À l'aide d'une boucle, ajouter les chiffres de 1 à `num`
+// - Appelez votre fonction avec le chiffre 12 et vérifiez que vous obtenez 78
+
+function addUp(num){
+    // while (1 <= num){
+    //     num++
+    // }
+    for (i = 1; i)
+}
+addUp(20);
