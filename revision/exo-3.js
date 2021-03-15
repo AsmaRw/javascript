7c945e2153b411e0b8c655abc49faad3ba67ad5c
asma <asma.rw09@gmail.com>
2021-03-15 17:27:30 +0100

    exo-3

---
+++ revision/exo-3.js
@@ -0,0 +1,18 @@
+// ## 03 - Palindrome
+
+// - Créez une fonction `checkPal` qui reçoit une string en paramètre
+// - La fonction retourne "Palindrome !" si le mot se lit dans les deux sens, sinon elle retourne "Nope"
+// - Vérifiez que vous obtenez un positif avec l'argument "racecar" mais pas avec "laptop"
+// function checkPal(string){
+//     var tab = string.split(" ")
+//     console.log(tab)
+//     tab.reverse();
+// }
+
+function checkPal(str){
+    console.log(str.length)
+
+    for(var i = 0; i <= str.length - 1; i++){
+    }
+}
+checkPal("laptop")
