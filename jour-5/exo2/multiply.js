e90011fdf0c65d7c611460bcca1cf6edc7a28060
asma <asma.rw09@gmail.com>
2021-03-17 16:37:43 +0100

    exo2 fini

--- jour-5/exo2/multiply.js
+++ jour-5/exo2/multiply.js
@@ -3,37 +3,33 @@
 function multiply(number) {
 
     var firstInput = parseFloat(number)
-    
+
     if (Number.isInteger(firstInput) == false) {
-    
+
         console.log("it's not a number integer")
-    
+
     } else {
 
         for (var i = 1; i <= 10; i++) {
-            
+
             console.log(firstInput, "x", i, '=', firstInput * i)
 
         }
 
     }
 }
-
-
-
-
 function addition(number) {
 
     var firstInput = parseFloat(number)
-    
+
     if (Number.isInteger(firstInput) == false) {
-    
+
         console.log("it's not a number integer")
-    
+
     } else {
 
         for (var i = 1; i <= 10; i++) {
-            
+
             console.log(firstInput, "+", i, '=', firstInput + i)
 
         }
