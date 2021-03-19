09a30eab53c5ebd8c1bb636edbe398552773ca98
asma <asma.rw09@gmail.com>
2021-03-19 16:26:56 +0100

    validator

--- jour-6/exo3/validator.js
+++ jour-6/exo3/validator.js
@@ -6,3 +6,49 @@
 //     - le username ne doit comporter que des lettres, chiffres et tirets (les petits)
 //     - le mot de passe doit contenir au moins 6 caractères, au moins une lettre et au moins un chiffre, et peut contenir des tirets
 // - Si tout est bon, elle retourne "All good !", sinon elle retourne "error"
+
+
+var prompt = require("prompt")
+
+prompt.start()
+
+function checkProfile() {
+
+    var properties = [
+        {
+            name: "email",
+            validator : /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/gm,
+            warning: "l'email n'est pas au bon format "
+        },
+        {
+            name: "username",
+            validator: /^[a-zA-Z\s\-]+$/, 
+            warning: "Le username ne doit contenir que des lettres, espaces et tirets"
+        },
+        {
+            name: "password",
+            validator : /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,50})/,
+            hidden: true
+        }
+    ];
+
+    prompt.start(); // démarre le prompt
+
+    function onErr(err) { // permet de gérer les erreurs
+        console.log(err);
+        return 1;
+    }
+
+    prompt.get(properties, function (err, res) {
+        if (err) {
+            return onErr(err);
+        }
+
+        console.log("ALL GOOD");
+        
+
+    });
+
+}
+
+checkProfile()
