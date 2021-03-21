ed999f8ff676d7aa3c9ddfd953f628233f82f2df
asma <asma.rw09@gmail.com>
2021-03-21 20:36:15 +0100

    jour-4 terminer

--- jour-4/exo-6.js
+++ jour-4/exo-6.js
@@ -1,6 +1,6 @@
 function format(num) {
     var wHours = num % 3600;
-    var hours = (num - wHour) / 3600;
+    var hours = (num - wHours) / 3600;
     var wMin = wHours % 60;
     var min = (wHours - wMin) / 60;
     var sec = wMin;
