421e9a9055086ea36e193d0cdaac7c0609a514f6
asma <asma.rw09@gmail.com>
2021-03-23 11:08:56 +0100

    number

--- jour-7/exo-4.js
+++ jour-7/exo-4.js
@@ -2,7 +2,7 @@ var array = [1, "toto", 34, "javascript", 8]
 
 var numbers = array.filter( function (elem){
     if (typeof elem === 'number' )
-    {return elem}
+    return elem
 })
 
 console.log(numbers)
