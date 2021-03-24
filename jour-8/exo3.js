db45d781350c62d604e5fb21c522dbafc3b4e08f
asma <asma.rw09@gmail.com>
2021-03-25 00:37:46 +0100

    exo3 fini

---
+++ jour-8/exo3.js
@@ -0,0 +1,16 @@
+var request = require("request")
+
+function catchPokemon(id){
+    request.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118", function (err, res, body){
+
+    var pokemon = JSON.parse(body)
+
+    console.log('id =', id)
+    console.log('name =', pokemon.results[id].name)
+
+
+
+    })
+}
+
+catchPokemon(9)
