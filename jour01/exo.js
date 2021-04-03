9f1c63273c43d1ac59c809cc130511b8180dfef0
asma <asma.rw09@gmail.com>
2021-04-03 19:31:49 +0200

    jour01

---
+++ jour01/exo.js
@@ -0,0 +1,37 @@
+console.log("Hello World !");
+var test;
+test = "My name is asma";
+console.log(test);
+
+var name;
+name = "asma";
+console.log('Nice to meet you ' + name);
+
+var testLength;
+testLength = "I'm very long !";
+console.log(testLength.length);
+
+var food;
+food = "croissant is meh";
+food = food.replace('meh', 'good');
+console.log(food);
+
+var basic;
+basic = "This is Cool !";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic, basicUp, basicDown);
+
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
+
+var age = 23
+var template ="I'm 23 years old"
+console.log(`I'm ${age} years old`);
+
+
+var changed = "Bonjour";
+changed= changed.replace(/o/g, "a");
+
+console.log(changed);
